dc99fee7f6fcce01ace55cd9690438693cc2d90d
Jhonaiker Blanco <blancojhonaiker@gmail.com>
2022-08-29 09:56:13 -0500

    template

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,28 @@
 // scripts/deploy_upgradeable_box.js
-const { ethers, upgrades } = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-    const TestUniswap = await ethers.getContractFactory("TestUniswap");
-    console.log("Deploying TestUniswap...");
-    const testUniswap = await upgrades.deployProxy(TestUniswap, [], {
-        initializer: "initialize",
-    });
-    await testUniswap.deployed();
-    console.log("testUniswap deployed to:", testUniswap.address);
+    const ERC20 = await ethers.getContractFactory("ERC20.sol");
+    const NFT = await ethers.getContractFactory("NFT.sol");
+    const Marketplace = await ethers.getContractFactory("Marketplace.sol")
+    
+    const erc20 = await ERC20.deploy([])
+    await erc20.deployed()
+
+    const nft = await NFT.deploy([erc20.address])
+    await nft.deployed()
+
+    const marketplace = await Marketplace.deploy([erc20.address, nft.address])
+    await marketplace.deployed()
+
+    console.log("ERC20 deployed to: ", erc20.address)
+    console.log("NFT deployed to: ", nft.address)
+    console.log("marketplace deployed to: ", marketplace.address)
 }
 
-main();
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ test/Marketplace.test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers} = require("hardhat");
+const { toWei, tokens } = require("./utils");
+const networkID = 137;
+
+describe("Marketplace", () => {
+
+});
