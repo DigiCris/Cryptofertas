107eb19ef8240121f70f3b228134a04b9df0d1d6
Leandro Alvarez <43589282+leomaker1993@users.noreply.github.com>
2022-08-29 20:04:29 -0500

    feat(enviroment): setup of environment

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades");
+
+module.exports = {
+	networks: {
+		hardhat: {
+			forking: {
+				url: process.env.PROVIDER_URL,
+				blockNumber: 26089200,
+			},
+			chainId: 137,
+		},
+		rinkeby: {
+			url: process.env.PROVIDER_URL,
+			accounts: [process.env.ACCOUNT_KEY],
+		},
+		live: {
+			url: process.env.PROVIDER_URL,
+			accounts: [process.env.ACCOUNT_KEY],
+		},
+	},
+	namedAccounts: {
+		deployer: 0,
+		feeRecipient: 1,
+		user: 2,
+	},
+	solidity: {
+		version: "0.8.7",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	mocha: {
+		timeout: 240000,
+	},
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+// scripts/deploy_upgradeable_box.js
+const { ethers } = require("hardhat");
+
+async function main() {
+    const ERC20 = await ethers.getContractFactory("ERC20.sol");
+    const NFT = await ethers.getContractFactory("NFT.sol");
+    const Marketplace = await ethers.getContractFactory("Marketplace.sol")
+    
+    const erc20 = await ERC20.deploy([])
+    await erc20.deployed()
+
+    const nft = await NFT.deploy(erc20.address)
+    await nft.deployed()
+
+    const marketplace = await Marketplace.deploy(erc20.address, nft.address)
+    await marketplace.deployed()
+
+    console.log("ERC20 deployed to: ", erc20.address)
+    console.log("NFT deployed to: ", nft.address)
+    console.log("marketplace deployed to: ", marketplace.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
