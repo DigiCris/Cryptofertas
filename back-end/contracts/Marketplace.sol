de7298e6aa7a6d9c3c9dba5dc3b866b082328b44
cmarchese <cmarchesetdiii@gmail.com>
2022-09-05 15:19:22 -0300

    feat/SmartContracts: First commit to the complete smart contracts. individual functions are working properly but some errors happen when I call buy from the marketplace and I'm still struggling with that.

--- back-end/contracts/Marketplace.sol
+++ back-end/contracts/Marketplace.sol
@@ -1,7 +1,273 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.7.0 <0.9.0;
 
+
+//deployed at= 0xcf107aE151d4c5A9Ec8dd0480Aa9e6b7aF9CbDD8
 interface INFTFACTORY
 {
-    
+    //information
+    function getPrice(uint256 _tokenId) external view returns(uint256 _price);
+    function expiration(uint256 _tokenId) external view returns(uint256);
+
+    // Wallets
+    function nftProvider(uint256) external returns (address);
+    function nftEmbasador(uint256) external returns (address);
+    function nftOwner(uint256) external returns (address);
+    function ownerOf(uint256) external view returns (address);
+    function nftDeveloper() external returns (address);
+    function nftDAO() external returns (address);
+    function nftCompany() external returns (address);
+    function nftUser() external returns (address);
+
+
+    //functions (write)
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+}
+
+interface IVESTING
+{
+    function mintWithVesting(address _to,uint256 _amount, uint256 _tokenId, uint256 _vestingTime) external returns(bool _success);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+contract CMARKETPLACE
+{
+    INFTFACTORY private i_nft_factory_Contract;
+    IVESTING private i_my_token_Contract;
+
+    //Tokenomics set in the constructor
+    uint32 public  OwnerBenefit; //90 %
+    uint32 public  ProviderBenefit; // 0.2% -> in first time selling this amount goes to companyBenefit
+    uint32 public  EmbasadorBenefit; // 2%
+    uint32 public  DeveloperBenefit; // 2.5%
+    uint32 public  DaoBenefit; // 1%
+    uint32 public  CompanyBenefit; // 2.3%
+    uint32 public  UserBenefit; // 2%
+
+    //addresses
+    address private Erc20; //address of marketplace
+    address private NftFactory; //address of marketplace
+    address private Owner;
+
+    // modifiers
+    modifier onlyOwner() 
+    {
+        require(msg.sender==Owner,"Your are not the market owner");
+        _;
+    }
+
+    //set tokenomics
+    function setOwnerBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+        OwnerBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+    function setProviderBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+        ProviderBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+    function setEmbasadorBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+        EmbasadorBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+   function setDeveloperBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+        DeveloperBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+   function setDaoBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+       DaoBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+   function setCompanyBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+       CompanyBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+    function setUserBenefit(uint32 _perThousand) onlyOwner external returns(bool _success)
+    {
+      UserBenefit=_perThousand;
+        _success=true;
+        return(_success);
+    }
+
+
+    // constructor
+    constructor(address _ERC20, address _NFT)
+    {
+        // interface to talk to
+        i_my_token_Contract=IVESTING(_ERC20);
+        i_nft_factory_Contract= INFTFACTORY(_NFT);
+
+        // benefits to the wallets (tokenimics)
+        OwnerBenefit=900; // 900 per 1000
+        ProviderBenefit=2; // 2 per 1000
+        EmbasadorBenefit=20; // 20 per 1000
+        DeveloperBenefit=25; // 25 per 1000
+        DaoBenefit=10; // 10 per 1000
+        UserBenefit=20; // 20 per 1000
+        CompanyBenefit=23; // 23 per 1000... (the rest after giving the tokens to each part)
+
+        Owner=msg.sender;
+
+    }
+
+
+    function getNftPrice(uint256 _tokenId) external view returns(uint256 _price)
+    {
+        _price=i_nft_factory_Contract.getPrice(_tokenId);
+        return(_price);
+    }
+
+    function transferERC20( address _to, uint256 _amount) external returns(bool _success)
+    {
+        _success=i_my_token_Contract.transferFrom(msg.sender, _to, _amount);
+        if(_success!=true)
+        {
+            revert();
+        }
+        return(_success);
+    }
+    function burnERC20(uint256 _amount) external returns(bool _success)
+    {
+        _success=i_my_token_Contract.transferFrom(msg.sender, address(0x06Eb67071a06E676b678F5dd3614D852C129d460), _amount);
+        if(_success!=true)
+        {
+            revert();
+        }
+        return(_success);
+    }
+
+    function mintERC20(address _to, uint256 _amount, uint256 _vestingTime,uint256 _tokenId) external returns(bool _success)
+    {
+        //uint256 _vestingTime= i_nft_factory_Contract.expiration(_tokenId);
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+        return(_success);
+    }
+
+    function getVestingTime(uint256 _tokenId) external view returns(uint256 _vestingTime)
+    {
+        _vestingTime= i_nft_factory_Contract.expiration(_tokenId);
+        return(_vestingTime);
+    }
+
+    function transferNft(address _from, uint256 _tokenId) external returns(bool _success)
+    {
+        address _to=msg.sender;
+        i_nft_factory_Contract.safeTransferFrom(_from, _to, _tokenId);
+        _success=true;
+        return(_success);
+    }
+
+    function getNftOwner(uint256 _tokenId) public view returns(address _owner)
+    {
+        _owner= i_nft_factory_Contract.ownerOf(_tokenId);
+        return(_owner);
+    }
+
+
+    function Buy(uint256 _tokenId) public returns(bool _success)
+    {
+        // previously to this function the seller should have given permision to this contract to transfer the NFT in the NFT factory contract by calling: sellNft(uint256 _tokenId, uint256 _price)
+        //Previously to this function the buyer should have given permision to this contract to  transfer the ERC20 token from their account in the ERC20 token contract by calling: approve(address spender, uint256 amount)// amount should be a huge value. sepnder is the address of this smart contract
+
+
+        uint256 _BurningAmount;
+
+        // we transfer the ERC20 tokens from the buyer to the 0x0 (we burn them)
+        _BurningAmount=i_nft_factory_Contract.getPrice(_tokenId);
+        _success=i_my_token_Contract.transferFrom(msg.sender, address(0x06Eb67071a06E676b678F5dd3614D852C129d460), _BurningAmount);
+        if(_success!=true)
+        {
+            revert();
+        }
+
+        // we transfer the NFT from the seller to the user
+        address _from=i_nft_factory_Contract.nftOwner(_tokenId);
+        address _to=msg.sender;
+        i_nft_factory_Contract.safeTransferFrom(_from, _to, _tokenId);
+
+
+        // then we mint ERC20 tokens to all the following addresses.
+
+        // minting to owner
+        _to=_from;
+        uint256 _amount=( _BurningAmount*OwnerBenefit ) /1000;
+        uint256 _vestingTime= i_nft_factory_Contract.expiration(_tokenId);
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+
+        // minting to Provider
+        _to=i_nft_factory_Contract.nftProvider(_tokenId);
+        _amount=( _BurningAmount*ProviderBenefit ) /1000;
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+
+        // minting to Embasador
+        _to=i_nft_factory_Contract.nftEmbasador(_tokenId);
+        _amount=( _BurningAmount*EmbasadorBenefit ) /1000;
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+        
+        // minting to Developer
+        _to=i_nft_factory_Contract.nftDeveloper();
+        _amount=( _BurningAmount*DeveloperBenefit ) /1000;
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+
+        // minting to DAO
+        _to=i_nft_factory_Contract.nftDAO();
+        _amount=( _BurningAmount*DaoBenefit ) /1000;
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+
+        // minting to Company
+        _to=i_nft_factory_Contract.nftCompany();
+        _amount=( _BurningAmount*CompanyBenefit ) /1000;
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+
+        // minting to User
+        _to=i_nft_factory_Contract.nftUser();
+        _amount=( _BurningAmount*UserBenefit ) /1000;
+        _success=i_my_token_Contract.mintWithVesting(_to, _amount, _tokenId, _vestingTime);
+        if(_success!=true)
+        {
+            revert();
+        }
+        return(_success);
+    }
+
 }
--- back-end/contracts/NFT.sol
+++ back-end/contracts/NFT.sol
@@ -1,40 +1,127 @@
 // SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IVESTING
+{
+    function NFT_claim(address _to, uint256 _tokenID) external returns (uint256 _new_amount);
+    function mintWithVesting(address _to,uint256 _amount, uint256 _tokenID, uint256 _vesting_time) external returns(bool success);
+}
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-pragma solidity >=0.7.0 <0.9.0;
+//deployed at=0x8Af96CED952F5B02A142fD1D1E5eA34eE32A9218
+contract CNFTFactory is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable 
+{
 
-contract CNFTFactory is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter public tokenAmount;
+    Counters.Counter private _tokenIdCounter;
     Counters.Counter public productsAmount;
     string public _companyName;
 
-    mapping (address => uint256[] ) private cuponsOwner;
-    mapping (uint256 => address) private nftProvider; 
-    mapping (uint256 => address) private nftEmbasador;
-    mapping (uint256 => address) private nftOwner; 
-    mapping (uint256 => bool) private firstSold; 
-    mapping (uint256 => uint256) private nftPrice; 
-    mapping (uint256 => uint8) private nftCategory; 
-    mapping (uint256 => bool) private isNftUsed; 
-    mapping (uint256 => bool) private inSale; 
-    mapping (uint256 => uint256) private nftExpiration; 
-    mapping (uint8 => uint256[] ) private categoryToToken; 
+    struct STokenAmountData {
+        uint tokenTotal;
+        uint tokenActives;
+        uint tokenUsed;
+    }
+
+    struct SDataToDisplay {
+        uint timeToExpirate;
+        STokenAmountData tokenAmountInformation;
+        string tokenURI;
+        uint tokenPrice;
+    }
+
+
+    ///////////////////////////////// Rolls /////////////////////////////////////////////////////////// 
+    //NftProvider= Person or company that will provide the service or product for the NFT
+    //NftOwner= current owner of the NFT
+    //NftEmbasador= Person who publishes the NFT. If the publisher is the provider, there is no embasador.
+    //NftDeveloper= The team developing the project
+    //NftDao= Investoland that will be our entry to production
+    //NftCompany= The company we will be creating.
+    //////////////////////////////// End of Rolls /////////////////////////////////////////////////////
+
+    // ERC20 token contract interface
+    IVESTING private i_my_token_Contract;
+    
+
+///////////////////////// NFT Information //////////////////////////////////////////////////////////
+    mapping (uint256 => address) public nftProvider; // tokenId -> Address of NftProvider (tick)
+    mapping (uint256 => address) public nftEmbasador; // tokenId -> Address of NftEmbasador (tick)
+    // there is already a function called ownerOf for this nftOwner we are using
+    mapping (uint256 => address) public nftOwner; // tokenId -> Address of NftOwner (rename of ownerOf) (tick)
+    mapping (uint256 => bool) private firstSold; // tokenId -> firstSold (identify if it is a first time selling)(tick)
+    mapping (uint256 => uint256) private price; // tokenId -> price (the actual selling price) (tick)
+    mapping (uint256 => bool) private used; // tokenId -> Used (identify if it is already used) (tick)
+    mapping (uint256 => bool) private inSale; // tokenId -> InSale (are we selling it or not?) (tick)
+    mapping (uint256 => uint256) public expiration; // tokenId -> expiration (last time to use the NFT?) (tick)
+    mapping (uint256 => uint8) private category; // (tick)
+    mapping (address => uint256[] ) private cuponsOwner; //(tick)
+    mapping (uint8 => uint256[] ) private categoryToToken; // (tick)
+///////////////////////// End of NFT Information ////////////////////////////////////////////////////
+
+    // Fixed wallets set in the constructor
+    address public NftDeveloper; // our wallet
+    address public NftDAO; // wallet of the DAO
+    address public NftCompany; //wallet of our company
+    address public NftUser; //wallet for user redistribution
+    address private Owner; // address of the owner of the SC
+    address private Marketplace; //address of marketplace
+    address private Erc20; //address of marketplace
+
+    function setMarketErc20(address _market, address _erc20) external
+    {
+        Marketplace=_market;
+        Erc20=_erc20;
+    }
+
+
+// Added by jhonaiker
     mapping (uint256 => string) private nftName;
     mapping (uint256 => string) private nftDescription;
     mapping (uint256 => string) private nftImage;
     mapping (uint256 => string) private nftProviderName;
-    
     mapping (uint256 => string) public nftMetadata;
     mapping (uint256 => uint256[] ) public ProductTokens; 
-    
-    constructor() ERC721("Token", "TOK"){
-        _companyName = "ENEFETON.COM";    
+    mapping (uint256 => string) public productMetadata;
+    mapping (uint256 => uint256[]) public productTokens;
+    mapping (uint256 => uint256) public tokenToProduct;
+    mapping(address => uint256[]) public tokensCreatedByUser; 
+    mapping(address => uint256[]) public productsCreatedByUser;
+    mapping(address => uint256[]) public productsOfUser;
+
+
+
+
+    // Modifiers
+    modifier onlyNftOwner(uint256 _token_id)  // this is the owner of the NFT
+    {
+        require(msg.sender == nftOwner[_token_id], "You are not the owner of the NFT");
+        _;
     }
+
+
+    
+    constructor(address _AddrContract) ERC721("Token", "TOK")
+    {
+        _companyName = "ENEFETON.COM"; 
+
+        // interface to talk to
+        i_my_token_Contract=IVESTING(_AddrContract);
+
+        //fixed wallets
+        NftDeveloper=0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2; // our wallet
+        NftDAO=0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db; // wallet of the DAO
+        NftCompany=0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB; //wallet of our company
+        NftUser=0x617F2E2fD72FD9D5503197092aC168c91465E7f2; // wallets for user's giveaways
+        Owner= msg.sender;//0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+    }
+
 
     function mint(        
             address _NftProvider, 
@@ -44,29 +131,232 @@ contract CNFTFactory is ERC721, ERC721URIStorage, Ownable {
             uint256 _price, 
             uint256 _secondsToExpire, 
             uint256 _maxCupons
-        ) public {
-            productsAmount.increment();
+        ) public returns(uint256 _tokenId){
             uint256 _productid = productsAmount.current();
+            productsAmount.increment();
+            productMetadata[_productid] = _urlMetadata;
+
           for(uint i = 0; i < _maxCupons; i++) {
-            tokenAmount.increment();
-            uint256 _tokenId = tokenAmount.current();
-            
+
+              _tokenId = _tokenIdCounter.current();
+              _tokenIdCounter.increment();
+
             nftMetadata[_tokenId]=_urlMetadata;
             nftProvider[_tokenId]=_NftProvider;
             nftEmbasador[_tokenId]=msg.sender; 
             nftOwner[_tokenId]=_NftProvider;
             firstSold[_tokenId]=true; 
-            isNftUsed[_tokenId]=false; 
-            nftPrice[_tokenId]=_price; 
-            nftExpiration[_tokenId]=block.timestamp+_secondsToExpire;
+            used[_tokenId]=false; 
+            price[_tokenId]=_price; 
+            expiration[_tokenId]=block.timestamp+_secondsToExpire;
             ProductTokens[_productid].push(_tokenId);
             categoryToToken[_category].push(_tokenId); 
             _safeMint(_NftProvider, _tokenId);
-            _setTokenURI(_tokenId,_urlMetadata);
             cuponsOwner[_NftProvider].push(_tokenId);
+            tokensCreatedByUser[_NftProvider].push(_tokenId);
+            tokenToProduct[_tokenId] = productsAmount.current();
+            _setTokenURI(_tokenId,_urlMetadata);
+            
         }    
 
+        productsOfUser[_NftProvider].push(productsAmount.current());
+        productsCreatedByUser[_NftProvider].push(productsAmount.current()); 
+        return(_tokenId);
+
     }
+
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+
+//functions by CMarchese
+// Set and get addresses
+
+
+    function SetInSale(uint256 _tokenId, bool _newValue) public onlyNftOwner(_tokenId) returns(bool _inSale)
+    {// in order to work properly for all marketplace we would need to errase inSale when revoking the approval
+        if(_newValue==true)
+        {
+            approve(Marketplace, _tokenId); // I modify inSale inside this function
+        }
+        else 
+        {
+            inSale[_tokenId]=false;
+        }   
+        return(_newValue);
+    }
+    function approve(address to, uint256 tokenId) public override 
+    {
+        bool _used=used[tokenId];
+        require(_used==false,"Your NFT has already been used, you are not allowed to sell it"); // If I skip this line I could sell the NFT after using it
+        address owner = ERC721.ownerOf(tokenId);
+        require(to != owner, "ERC721: approval to current owner");
+        require(_msgSender() == owner || isApprovedForAll(owner, _msgSender()),"ERC721: approve caller is not token owner nor approved for all");
+        inSale[tokenId]=true;
+        _approve(to, tokenId);
+    }    
+
+    function sellNft(uint256 _tokenId, uint256 _price) onlyNftOwner(_tokenId) external returns(uint256)
+    {
+        price[_tokenId]=_price;
+        SetInSale(_tokenId, true);
+        return(_price);
+    }
+
+    function MarkUsed(uint256 _tokenId) external onlyNftOwner(_tokenId) returns(bool _success)
+    {
+        // It can only be used once
+        bool _used=used[_tokenId];
+        require(_used==false,"Your NFT has already been used");
+        inSale[_tokenId]=false;
+        used[_tokenId]=true;
+
+        // We release the vestings associated with this NFT
+        i_my_token_Contract.NFT_claim(nftProvider[_tokenId], _tokenId);
+        i_my_token_Contract.NFT_claim(nftEmbasador[_tokenId], _tokenId);
+        i_my_token_Contract.NFT_claim(nftOwner[_tokenId], _tokenId);
+        i_my_token_Contract.NFT_claim(NftDeveloper, _tokenId);
+        i_my_token_Contract.NFT_claim(NftDAO, _tokenId);
+        i_my_token_Contract.NFT_claim(NftCompany, _tokenId);
+        
+        _success=true;
+        return(_success);
+    }
+
+    function getPrice(uint256 _tokenId) public view returns(uint256 _price)
+    {
+        _price=price[_tokenId];
+        return(_price);
+    }
+
+    function getTokensByCategory(uint8 _category) public view returns(uint256[] memory)
+    {
+        return(categoryToToken[_category]);
+    }
+
+    function getCuponsOwner(address _addr) public view returns(uint256[] memory)
+    {
+        return(cuponsOwner[_addr]);
+    }
+
+// Functions by jhonaiker
+    function getTokenAmount(uint256 _productId) private view returns (STokenAmountData memory tokenAmountStruct) {
+        uint256[] memory _tokenList = productTokens[_productId];
+        uint256 _tokenTotal = _tokenList.length;
+        uint256 _activesTokens = 0;
+
+        for(uint256 i = 0; i < _tokenTotal; i++) {
+            if(used[_tokenList[i]] == false) {
+                _activesTokens++;
+            }
+        }
+
+        return STokenAmountData(_tokenTotal, _activesTokens, _tokenTotal - _activesTokens);
+    }
+
+    function getTokenAmountOfBuyer(address _user,uint256 _productId) private view returns (STokenAmountData memory tokenAmountStruct){
+        uint256[] memory _listOfTokensOfCurrentProduct = productTokens[_productId]; 
+        uint256 _amountOfTokensOfCurrentUser = 0;
+        uint256 _amountOfActiveTokensOfCurrentUser = 0;
+
+        for(uint256 i = 0; i < _listOfTokensOfCurrentProduct.length; i++) {
+            if(nftOwner[_listOfTokensOfCurrentProduct[i]] == _user) {
+                if(used[_listOfTokensOfCurrentProduct[i]] == false) {
+                    _amountOfActiveTokensOfCurrentUser++;
+                }
+                _amountOfTokensOfCurrentUser++;
+            }
+        }
+
+        return STokenAmountData(_amountOfTokensOfCurrentUser, _amountOfActiveTokensOfCurrentUser, _amountOfTokensOfCurrentUser - _amountOfActiveTokensOfCurrentUser);
+
+    }
+
+    function getDataToDisplayForOwner(address _user) public view returns(SDataToDisplay[] memory _result){
+        uint256[] memory _listOfProductsOfCurrentUser = productsOfUser[_user];
+        SDataToDisplay[] memory _data = new SDataToDisplay[](_listOfProductsOfCurrentUser.length);
+
+        for(uint256 i = 0; i < _listOfProductsOfCurrentUser.length; i++){
+            uint256 _timeToExpirate = getTimeToExpirationOfProduct(_listOfProductsOfCurrentUser[i]);
+            STokenAmountData memory _tokenAmountData = getTokenAmountOfBuyer(_user, _listOfProductsOfCurrentUser[i]);
+            string memory _productURI = getTokenURIOfProduct(_listOfProductsOfCurrentUser[i]);
+            uint256 _productPrice = getProductPrice(_listOfProductsOfCurrentUser[i]);
+            _data[i] = SDataToDisplay(_timeToExpirate, _tokenAmountData, _productURI, _productPrice);
+        }
+
+        return _data;
+    }    
+
+    function getDataToDisplayForCreator(address _user) public view returns(SDataToDisplay[] memory _result){
+        uint256[] memory _userProducts = productsCreatedByUser[_user];
+       SDataToDisplay[] memory _data = new SDataToDisplay[](_userProducts.length);
+
+       for(uint i = 0; i < _userProducts.length; i++) {
+           uint256 _timeToExpirate = getTimeToExpirationOfProduct(_userProducts[i]);
+           STokenAmountData memory _tokenAmountData = getTokenAmount(_userProducts[i]);
+           string memory _productURI = getTokenURIOfProduct(_userProducts[i]);
+           uint256 _productPrice = getProductPrice(_userProducts[i]);
+
+           _data[i] = SDataToDisplay(_timeToExpirate, _tokenAmountData, _productURI, _productPrice);
+
+       }
+
+       return _data;
+    }
+
+    function getTimeToExpirationOfProduct(uint256 _productId) private view returns (uint256 _result) {
+        uint256[] memory _tokenList = productTokens[_productId];
+        uint256 _firstToken = _tokenList[0];
+        uint256 _expirationOfCurrentToken = expiration[_firstToken];
+        uint256 _timeToExpiration = 0;
+
+        if(_expirationOfCurrentToken > block.timestamp) {
+            _timeToExpiration = _expirationOfCurrentToken - block.timestamp;
+        }
+
+        return _timeToExpiration;
+
+    }
+
+    function getTokenURIOfProduct(uint256 _productId) private view returns (string memory _result) {
+        uint256[] memory _tokenList = productTokens[_productId];
+        uint256 _firstToken = _tokenList[0];
+
+        return nftMetadata[_firstToken];
+    }
+
+    function checkIfNumberAlreadyIsInArray(uint256[] memory _arrayOfNumber, uint256 _newNumberToArray) private pure returns(bool) {
+        bool _result = false;
+        for(uint256 i = 0; i < _arrayOfNumber.length; i++) {
+            if(_arrayOfNumber[i] == _newNumberToArray){
+                _result = true;
+                break;
+            }
+        }
+
+        return _result;
+    }
+
+/*
+    function useToken(uint256 _tokenId) public {
+        used[_tokenId] = true;
+    }// the proper function is  MarkUsed(uint256 _tokenId)
+*/
+
+    function getProductPrice (uint256 _productId) private view returns (uint256 _result) {
+        uint256[] memory _tokenList = productTokens[_productId];
+        uint256 _firstToken = _tokenList[0];
+
+        return price[_firstToken];        
+    } // I didn't errase this function because it is just a view but you should use getPrice(uint256 _tokenId), not this one
+
+
+
 
 
 // The following functions are overrides required by Solidity.
@@ -84,7 +374,30 @@ contract CNFTFactory is ERC721, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
-        
+
+
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        firstSold[tokenId]=false;
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _afterTokenTransfer(address /*from*/, address /*to*/, uint256 tokenId ) internal override 
+    {
+        nftOwner[tokenId]=ownerOf(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }        
 
 
 
