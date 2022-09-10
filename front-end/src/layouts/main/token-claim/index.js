58070c4c4002f2ca9a2a47a591adc688d08302f6
cmarchese <cmarchesetdiii@gmail.com>
2022-09-10 16:09:42 -0300

    feat/claim Vesting/cmarchese: claim button already working but after claiming localhost goes blank and I have to refresh the page.

--- front-end/src/layouts/main/token-claim/index.js
+++ front-end/src/layouts/main/token-claim/index.js
@@ -31,45 +31,94 @@ const TokenClaim = () => {
 
   async function balanceOf()
   {
+    
     let totalVesting= await erc20.methods.vestingQuantity(account).call();
     //await setBalance(vestq);
-    totalVesting=cortar_numero(totalVesting);
+    //alert("totalVesting before="+totalVesting);
+    totalVesting=Acondicionar_numero(totalVesting);
+    //alert("totalVesting after="+totalVesting);
     var acumulatedVesting=0;
     var toHarvest=0;
-    
+
     for(let i=0;totalVesting>acumulatedVesting; i++) // my idea is to loop the vesting dates until I reach the total quantity and I only count the dates which are set to 0. the acumulated of that would be what I am allowed to claim
     {
       var {0:vest, 1:time}= await erc20.methods.getVestingDates(account , i).call();
-      while(cortar_numero(vest)==NaN)
-      {
-        i++;
-        var {0:vest, 1:time}= await erc20.methods.getVestingDates(account , i).call();
-      }
-      console.log("vest="+vest); 
+      //alert("vest before="+vest);
+      vest=Acondicionar_numero(vest);
+      //alert("vest after="+vest);
 
-      acumulatedVesting+=cortar_numero(vest);  
-      acumulatedVesting+=totalVesting+0.01; // to solve problem of truncating numbers
+      //alert("acumulatedVesting before= "+acumulatedVesting);
+      acumulatedVesting=parseFloat( parseFloat(acumulatedVesting)+parseFloat(vest) );  
+      //alert("acumulatedVesting after add= "+acumulatedVesting);
+      acumulatedVesting=parseFloat( parseFloat(acumulatedVesting)+parseFloat(0.01) ); // to solve problem of truncating numbers
+      //alert("acumulatedVesting + 0.1 ="+acumulatedVesting);
+      
       if(time==0)
       {
-        toHarvest=toHarvest+cortar_numero(vest);
+        toHarvest=parseFloat( parseFloat(toHarvest)+parseFloat(vest) );
       }
-      console.log("acumulatedVesting="+acumulatedVesting); 
     }
+    //toHarvest=10.32;
     console.log("toHarvest="+toHarvest); 
     await setBalance(toHarvest);
     //await setBalance( (balance / 1e18).toFixed(2) );
+  
   }
 
-function cortar_numero(numero)
+
+function lookFor(numero,character)
 {
-  numero=numero.substring(0,(numero.length-16));
-  return (parseFloat(numero)/100);
+  for(let i=0; i<=numero.length; i++)
+    {
+        if(numero[i]==character)
+        {
+            return(true);
+        }
+    }
+    return(false);
+}
+
+function Acondicionar_numero(numero)
+{ // don't send an integer number already treated or it would take it as a non treated string. isNaN was not working to make the distinction
+  //alert("entre a cortar numero");
+  numero=numero.toString();
+  //alert("stringed= "+numero);
+
+  // here all the numers are string
+  if(lookFor(numero,'.'))
+  {
+    numero=parseFloat(numero).toFixed(2);
+    return( parseFloat(numero) );
+  }
+  else
+  {
+   //alert("I didnot find .');
+  }
+  numero = numero.replace('.', '');
+  numero = numero.replace(',', '');
+  //alert("si tenia . o , se lo saco y queda= "+numero);
+  // if the string had a . or a , for decimal numbers I errase them
+  while(numero.length<20)
+  {
+    numero='0'+numero;
+  }
+  //alert("al menos tendrá 20 digitos= "+numero);
+  // here all the numbers are strings with at least 20 numbers
+  numero=numero.substring(0,(numero.length-16)); // I take out the last 16 numbers so I would leave only 2 decimal digits
+
+  //alert("lo recorto y le dejo solo 2 digitos decimales= "+numero);
+
+  numero=parseFloat(numero)/100;  // I change it into a numerb again and divide it by 100 to get the decimal digits in a proper way
+
+  //alert("lo divido y lo transformo en un numero= "+numero);
+
+  return(parseFloat(numero));
 }
 
 
   async function claim()
   {
-    erc20.methods.claim(0).send({from: account})
+    erc20.methods.claim(31).send({from: account})
       .on('transactionHash', 
       function(hash)
       {
@@ -82,7 +131,7 @@ function cortar_numero(numero)
       function(receipt)
       {
         toast({
-          title: 'Transaccion done.',
+          title: 'Your tokens have been claimed. Enjoy!!!.',
           description: receipt,
           status: 'success',});
       })
