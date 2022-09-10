b98b816781caf33b1e46f872d5d161a5c7541312
cmarchese <cmarchesetdiii@gmail.com>
2022-09-10 17:11:25 -0300

    Checked and still going blank using the await inside the async of the claiming function

--- front-end/src/layouts/main/token-claim/index.js
+++ front-end/src/layouts/main/token-claim/index.js
@@ -118,7 +118,7 @@ function Acondicionar_numero(numero)
 
   async function claim()
   {
-    await erc20.methods.claim(31).send({from: account})
+    await erc20.methods.claim(32).send({from: account})
       .on('transactionHash', 
       function(hash)
       {
